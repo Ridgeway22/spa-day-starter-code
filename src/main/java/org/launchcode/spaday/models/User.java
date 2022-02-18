683b637d3094cc4acca1102aca6732f55d2c2b13
Ridgeway22 <boo22mickey@yahoo.com>
2022-02-18 08:01:01 -0600

    git commit -m "assignment done"

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,42 @@
+package org.launchcode.spaday.models;
+
+public class User {
+    private static String userName;
+    private static String email;
+
+    private static String password;
+
+    public User() {
+    }
+
+    public User(String userName, String email, String password) {
+        this();
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        User.userName = userName;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        User.email = email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+}
