959354bd969f318ad04ceedc4397609ecd94c3a9
Ridgeway22 <boo22mickey@yahoo.com>
2022-02-21 21:08:57 -0600

    git commit -m "studio 14 day 12"

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,21 +1,46 @@
 package org.launchcode.spaday.models;
 
-public class User {
-    private String username;
-    private String email;
-    private String password;
+import javax.validation.constraints.*;
 
+public class User {
+    @NotEmpty(message = "please enter a name")
+    @Size(min=5,max= 15)
+    private String username;
+
+    @Email(message = "PLease provide a valid email address")
+    private String email;
+
+    @NotEmpty(message="please add a 6min password")
+    @Size(min=6 )
+    private String password;
+@NotNull(message = "passwords do not match")
+    private String verifyPassword;
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if(!this.password.isEmpty() && (this.getVerifyPassword() != null) && !this.password.equals(this.getVerifyPassword())){
+            verifyPassword = null;
+        }
+    }
     public String getUsername() {
         return username;
     }
@@ -38,5 +63,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
 }
